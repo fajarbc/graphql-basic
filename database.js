9f97a5fa24c2f8f639a363c3bb779e87a784f313
Fajar BC <bcfajar@gmail.com>
2022-07-28 22:59:05 +0700

    refactor: resolvers only call function from database

--- database.js
+++ database.js
@@ -3,4 +3,49 @@ const FileSync = require("lowdb/adapters/FileSync");
 
 const adapter = new FileSync("database.json");
 const db = low(adapter);
-module.exports = { db };
+
+module.exports = {
+  db,
+  getUserById: (userId) => {
+    const Users = db.get("users").value();
+    const index = Users.findIndex((user) => user.id == userId);
+    return Users[index];
+  },
+  follow: (userId1, userId2) => {
+      // "u1" follows "u2"
+      const u1 = db.get("users").find({ id: Number(userId1) });
+      const u1Val = {...u1.value()};
+      const u2 = db.get("users").find({ id: Number(userId2) });
+      const u2Val = {...u2.value()};
+      if (u1Val === undefined || u2Val === undefined) return null;
+
+      // write "following" to "u1" data
+      const following = u1Val.following ?? [];
+      delete u2Val["followers"];
+      delete u2Val["following"];
+      if(following.findIndex(item => item.id == u2Val.id) == -1) following.push(u2Val);
+      u1.assign({ following: following }).write();
+
+      // write "followers" to "u2" data
+      const followers = u2Val.followers ?? [];
+      delete u1Val["followers"];
+      delete u1Val["following"];
+      if(followers.findIndex(item => item.id == u1Val.id) == -1) followers.push(u1Val);
+      u2.assign({ followers: followers }).write();
+
+      return u1.value();
+  },
+  getPostById: (postId) => {
+    const Posts = db.get("posts").value();
+    const index = Posts.findIndex((post) => post.id == postId);
+    return Posts[index];
+  },
+  getUserPost: (userId) => {
+    const Posts = db.get("posts").value();
+    let result = [];
+    Posts.map((post) => {
+      if (post.user.id == userId) result.push(post);
+    });
+    return result;
+  },
+};
--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,4 +1,4 @@
-const { db } = require("../database");
+const { db, follow, getUserById, getPostById, getUserPost } = require("../database");
 const fs = require("fs");
 
 const resolvers = {
@@ -7,49 +7,20 @@ const resolvers = {
       return db.get("users").value();
     },
     user: (_, args) => {
-      const Users = db.get("users").value();
-      const index = Users.findIndex((user) => user.id == args.id);
-      return Users[index];
+      return getUserById(args.id)
     },
     follow: (_, args) => {
       // "u1" follows "u2"
-      const u1 = db.get("users").find({ id: Number(args.input.userId1) });
-      const u1Val = {...u1.value()};
-      const u2 = db.get("users").find({ id: Number(args.input.userId2) });
-      const u2Val = {...u2.value()};
-      if (u1Val === undefined || u2Val === undefined) return null;
-
-      // write "following" to "u1" data
-      const following = u1Val.following ?? [];
-      delete u2Val["followers"];
-      delete u2Val["following"];
-      if(following.findIndex(item => item.id == u2Val.id) == -1) following.push(u2Val);
-      u1.assign({ following: following }).write();
-
-      // write "followers" to "u2" data
-      const followers = u2Val.followers ?? [];
-      delete u1Val["followers"];
-      delete u1Val["following"];
-      if(followers.findIndex(item => item.id == u1Val.id) == -1) followers.push(u1Val);
-      u2.assign({ followers: followers }).write();
-
-      return u1.value();
+      return follow(args.input.userId1, args.input.userId2)
     },
     posts: () => {
       return db.get("posts").value();
     },
     post: (_, args) => {
-      const Posts = db.get("posts").value();
-      const index = Posts.findIndex((post) => post.id == args.id);
-      return Posts[index];
+      return getPostById(args.id)
     },
     userPosts: (_, args) => {
-      const Posts = db.get("posts").value();
-      let result = []
-      Posts.map((post) => {
-        if(post.user.id == args.userId) result.push(post)
-      });
-      return result;
+      return getUserPost(args.userId)
     },
   },
 };
